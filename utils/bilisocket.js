a61d0f11ba48f639154ec5b7372b31564ff85866
Caoliang <i@caoliang.net>
2018-12-20 19:32:55 +0800

    new logger.

---
+++ utils/bilisocket.js
@@ -0,0 +1,139 @@
+let W3CWebSocket = require('websocket').w3cwebsocket;
+
+function generatePacket (action, payload) {
+    payload = payload || '';
+    let packetLength = Buffer.byteLength(payload) + 16;
+    let buff = new Buffer.alloc(packetLength);
+
+    buff.writeInt32BE(packetLength, 0);
+    // write consts
+    buff.writeInt16BE(16, 4);
+    buff.writeInt16BE(1, 6);
+    buff.writeInt32BE(1, 12);
+    // write action
+    buff.writeInt32BE(action, 8);
+    // write payload
+    buff.write(payload, 16);
+    return buff
+}
+
+function sendJoinRoom(client, rid){
+    let uid = 1E15 + Math.floor(2E15 * Math.random());
+    let packet = JSON.stringify({uid: uid, roomid: rid});
+    let joinedRoomPayload = generatePacket(7, packet);
+    client.send(joinedRoomPayload);
+}
+
+function parseMessage(arrayBuffer, room_id, procFn, failedFn){
+    if(arrayBuffer.byteLength < 21) {return}
+
+    let buff = Buffer.from(arrayBuffer);
+    let view = new Uint8Array(arrayBuffer);
+    for (let i = 0; i < buff.length; ++i) {buff[i] = view[i]}
+    while (buff.length > 16){
+        let length = (buff[0] << 24) + (buff[1] << 16) + (buff[2] << 8) + buff[3];
+        let current = buff.slice(0, length);
+        buff = buff.slice(length);
+        if (current.length > 16 && current[16] !== 0){
+            try{
+                let msg = JSON.parse("" + current.slice(16));
+                procFn(msg, room_id);
+            }catch (e) {
+                (failedFn || function(e){})("e: " + current);
+            }
+        }
+    }
+}
+
+
+let HEART_BEAT = generatePacket(2);
+let MONITOR_URL = "ws://broadcastlv.chat.bilibili.com:2244/sub";
+
+exports.MONITOR_URL = MONITOR_URL;
+exports.HEART_BEAT_PACKAGE = HEART_BEAT;
+exports.parseMessage = parseMessage;
+exports.sendJoinRoom = sendJoinRoom;
+
+
+class ConnectionPool {
+    constructor(logger) {
+        this.message_count = 0;
+        this.current_connections = {};
+        this.logging = logger;
+    }
+    reConnect(room_id){
+        let client = new W3CWebSocket(MONITOR_URL);
+        this.current_connections[room_id] = client;
+        client.onerror = () => {
+            this.onConnectionError(client);
+        };
+        client.onopen = () => {
+            this.onConnectionOpen(client, room_id);
+        };
+        client.onclose = () => {
+            this.onConnectionClose(client, room_id);
+        };
+        this.logging.info("Connection: " + room_id + " RECONNECTED!");
+    }
+    onConnectionError(client, room_id){
+        if(!(room_id in this.current_connections)){
+            this.logging.error('Connection Removed (EXPECTED, but caused by error), room id: ' + room_id);
+            return ;
+        }
+        let existedClient = this.current_connections[room_id];
+        if(existedClient === undefined){
+            this.logging.error('Connection Error happened at creating, room id: ' + room_id);
+        }else{
+            if (existedClient === client){
+                this.logging.error('UNEXPECTED Connection Error happened, room id: ' + room_id);
+                let reConnect = this.reConnect;
+                setTimeout(function(){
+                    reConnect(room_id)
+                }, Math.random()*10000);
+            }else{
+                this.logging.error('Connection Removed (EXPECTED, but caused by duplicated!), room id: ' + room_id);
+            }
+        }
+    }
+    onConnectionOpen(client, room_id){
+        sendJoinRoom(client, room_id);
+        function sendHeartBeat() {
+            if (client.readyState === client.OPEN) {
+                client.send(HEART_BEAT_PACKAGE);
+                setTimeout(sendHeartBeat, 10000);
+            }
+        }
+        sendHeartBeat(true);
+    }
+    onConnectionClose(client, room_id){
+        let existedClient = this.current_connections[room_id];
+        if(existedClient && existedClient === client) {
+            this.logging.error('Connection UNEXPECTED closed: ' + room_id);
+            let reConnect = this.reConnect;
+            setTimeout(function () {
+                reConnect(room_id)
+            }, Math.random() * 10000);
+        }
+    }
+    addNewConnection = (room_id) => {
+        let existedClient = this.current_connections[room_id];
+        if(existedClient !== undefined){
+            this.logging.error("BAD CODE! do not create duplicated client.");
+            return
+        }
+
+        let client = new W3CWebSocket(MONITOR_URL);
+        this.current_connections[room_id] = client;
+        client.onerror = () => {
+            this.onConnectionError(client);
+        };
+        client.onopen = () => {
+            this.onConnectionOpen(client, room_id);
+        };
+        client.onclose = () => {
+            this.onConnectionClose(client, room_id);
+        };
+    }
+}
+
+exports.ConnectionPool = ConnectionPool;
---
+++ utils/logger.js
@@ -0,0 +1,18 @@
+let log4js = require("log4js");
+let path = require('path');
+
+exports.creatLogger = function(loggerName, path_){
+    let config = {
+        appenders: {console: { type: 'console' }},
+        categories: {default: { appenders: ['console'], level: 'trace' }}
+    };
+    config.appenders[loggerName] = {
+        type: 'file',
+        filename: path.join(path_, loggerName+ ".log"),
+        maxLogSize: 1024*1024*50,
+        backups: 2,
+    };
+    config.categories[loggerName] = {appenders: [loggerName, 'console'], level: 'ALL' };
+    log4js.configure(config);
+    return log4js.getLogger(loggerName);
+};
