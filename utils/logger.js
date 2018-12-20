b0e014974f639921272d1d1f08bdfb70eb6e9bf9
Caoliang <i@caoliang.net>
2018-12-20 22:33:52 +0800

    unlimit;

--- utils/logger.js
+++ utils/logger.js
@@ -9,7 +9,7 @@ exports.creatLogger = function(loggerName, path_){
     config.appenders[loggerName] = {
         type: 'file',
         filename: path.join(path_, loggerName+ ".log"),
-        maxLogSize: 1024*1024*50,
+        maxLogSize: 1024*1024*200,
         backups: 2,
     };
     config.categories[loggerName] = {appenders: [loggerName, 'console'], level: 'ALL' };
