4ad96139bca41f51fd2a89d582398201cb7b19a1
Caoliang <i@caoliang.net>
2018-12-23 14:37:51 +0800

    new heart beat.

--- heartbeat.js
+++ heartbeat.js
@@ -1,44 +1,18 @@
 let request = require("request");
 let fs = require('fs');
-let log4js = require('log4js');
+let logger = require("./utils/logger");
 let DEBUG = !(process.argv.splice(2)[0] === "server");
 
-let logerconf = {
-  appenders: {
-    heartbeat: {
-      type: 'file',
-      filename: DEBUG ? './log/heartbeat.log' : "/home/wwwroot/log/heartbeat.log",
-      maxLogSize: 1024*1024*50,
-      backups: 2,
-    },
-    console: { type: 'console' }
-  },
-  categories: {
-    heartbeat: { appenders: ['heartbeat', 'console'], level: 'ALL' },
-    default: { appenders: ['console'], level: 'trace' }
-  }
-};
-log4js.configure(logerconf);
-let logging = log4js.getLogger('heartbeat');
 
-let cookie_filename = DEBUG ? './cookie.txt' : "/home/wwwroot/notebook.madliar/notebook_user/i@caoliang.net/cookie.txt";
-let cookie = fs.readFileSync(cookie_filename, "utf-8");
-let cookie_kv = cookie.split(";");
-let csrf_token = "";
-for (let i = 0; i < cookie_kv.length; i++){
-    let kvstr = cookie_kv[i];
-    if (kvstr.indexOf("bili_jct") > -1){
-        csrf_token = kvstr.split("=")[1].trim();
-        break;
-    }
-}
+let logging = logger.creatLogger('heartbeat', DEBUG ? "./log/" : "/home/wwwroot/log/");
 let UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36';
-let headers = {"User-Agent": UA, "Cookie": cookie};
+let RAW_COOKIES_LIST = require('./data/cookie.js').RAW_COOKIE_LIST;
 
-function heartbeat_90s(){
+
+function heartbeat_90s(cookie){
     let reqParam = {
         url: "https://api.live.bilibili.com/relation/v1/Feed/heartBeat",
-        headers: headers,
+        headers: {"User-Agent": UA, "Cookie": cookie},
         timeout: 5000,
     };
     request.get(reqParam, function (err, res, body) {
@@ -49,37 +23,52 @@ function heartbeat_90s(){
         }
     });
 }
-function postLatestTime(){
+function postLatestTime(cookie, index){
     let timest = (new Date()).valueOf();
     request.get({
         url: "https://api.live.bilibili.com/relation/v1/feed/heartBeat?_=" + timest,
-        headers: headers,
+        headers: {"User-Agent": UA, "Cookie": cookie},
         timeout: 5000,
     }, function (err, res, body) {
         if (err) {
-            logging.error("90s Heartbeat send error! ", err.toString());
+            logging.error("90s Heartbeat send error, index: %d, e: %s", index, err.toString());
         }else{
-            logging.info("Heartbeat (" + timest + ")POST: " + body.toString());
+            let r = JSON.parse(body.toString());
+            if(r.code === 0){
+                logging.info("postLatestTime Success! index: %d, timest: %s", index, timest);
+            }else{
+                logging.info("postLatestTime Error! index: %d, r: %s", index, body.toString());
+            }
         }
     });
 }
-function heartbeat_5m(){
+function heartbeat_5m(cookie, index){
     request.post({
         url: "https://api.live.bilibili.com/User/userOnlineHeart",
-        headers: headers,
+        headers: {"User-Agent": UA, "Cookie": cookie},
         timeout: 5000,
     }, function (err, res, body) {
         if (err) {
-            logging.error("5m Heartbeat send error! ", err.toString());
+            logging.error(
+                "5m Heartbeat send error! index: %s, e: %s, body: %s",
+                index, err.toString(), body.toString()
+            );
         }else{
-            logging.info("Heartbeat 5m: " + body.toString());
+            let r = JSON.parse(body.toString());
+            if(r.code === 0){
+                logging.info("Send Heartbeat Success! index: %d", index);
+            }else{
+                logging.info("Error happened in 5m, index: %d, r: %s", index, body.toString());
+            }
         }
-        postLatestTime();
+        postLatestTime(cookie, index);
     });
 }
 
-heartbeat_5m();
-
-// setInterval(heartbeat_90s, 1000*90);
-// setInterval(heartbeat_10m, 1000*60*10);
-// setInterval(heartbeat_20m, 1000*60*2);
+(() => {
+    logging.info("Start send heartbeat proc, ENV: %s", DEBUG ? "DEBUG": "SERVER");
+    for (let i = 1; i < RAW_COOKIES_LIST.length; i++){
+        let c = RAW_COOKIES_LIST[i];
+        heartbeat_5m(c, i);
+    }
+})();
