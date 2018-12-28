a7f2e9dff2e4ff5d5879f04786c6871555029d98
Caoliang <i@caoliang.net>
2018-12-28 14:12:37 +0800

    js handler.

--- utils/acceptprize.js
+++ utils/acceptprize.js
@@ -140,8 +140,8 @@ class Acceptor {
                                 sender = gidlist[i].from;
                             if (gid !== 0){
                                 logging.info(
-                                    "\t\t Delay %d msecs to join TV prize, room_id: %s, gid: %s, title: %s, sender: %s",
-                                    delayTime, room_id, gid, title, sender
+                                    "\t\t Delay %.3f msecs to join TV prize, room_id: %s, gid: %s, title: %s, sender: %s",
+                                    delayTime/1000, room_id, gid, title, sender
                                 );
                                 setTimeout(() => {joinFn(gid, title, sender)}, delayTime);
                             }
@@ -151,7 +151,7 @@ class Acceptor {
             })
         };
         let delayTime = parseInt((index === 0 ? 10 : 60)*Math.random()*1000);
-        logging.info("\t\t Delay %d milliseconds to get TV gift id, room_id: %s", delayTime, room_id);
+        logging.info("\t\t Delay %.3f milliseconds to get TV gift id, room_id: %s", delayTime/1000, room_id);
         setTimeout(() => {getTvGiftId(room_id)}, delayTime);
     }
     acceptTv(room_id){
