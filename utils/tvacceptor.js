7c1c8a5e5ad9a2a7e05aae369a914b5555fbb315
Caoliang <i@caoliang.net>
2018-12-29 11:59:14 +0800

    new acceptor.

--- utils/tvacceptor.js
+++ utils/tvacceptor.js
@@ -73,7 +73,7 @@ let Acceptor = {
                 if (Acceptor.__GIFT_ID_POOL.indexOf(k) < 0){
                     Acceptor.__GIFT_ID_POOL.push(k);
                     if (Acceptor.__joinTVDispatcherTask === 0){
-                        Acceptor.__joinTVDispatcherTask = setInterval(Acceptor.__joinTVDispatcher, 1000);
+                        Acceptor.__joinTVDispatcherTask = setInterval(Acceptor.__joinTVDispatcher, 500);
                         Acceptor.defaultLogger.info(
                             "Start __joinTVDispatcher task, task id: %s.", Acceptor.__joinTVDispatcherTask
                         );
