ec92bdf6c48e83be186e7ada25364a59d192e1c1
Caoliang <i@caoliang.net>
2019-02-04 18:21:23 +0800

    use python.

--- test.py
+++
@@ -1,9 +0,0 @@
-from config.log4 import acceptor_logger
-import socket
-from config import config
-PRIZE_SOURCE_PUSH_ADDR = tuple(config["PRIZE_SOURCE_PUSH_ADDR"])
-print(PRIZE_SOURCE_PUSH_ADDR)
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.sendto("123".encode("utf-8"), ("localhost", 11111))
-s.close()
