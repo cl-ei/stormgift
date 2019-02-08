4fa19db815aba15a2be69e98c25394cd9a99d95b
Caoliang <i@caoliang.net>
2019-02-08 11:45:58 +0800

    use python.

--- node/loggers.js
+++ node/loggers.js
@@ -36,48 +36,6 @@ let config = {
             maxLogSize: 1024*1024*50,
             backups: 2,
         },
-        acceptor: {
-            type: 'file',
-            filename: path.join(loggerFilePath, "acceptor.log"),
-            maxLogSize: 1024*1024*50,
-            backups: 2,
-        },
-        apz_default: {
-            type: 'file',
-            filename: path.join(loggerFilePath, "apz_default.log"),
-            maxLogSize: 1024*1024*50,
-            backups: 2,
-        },
-        apz_tv: {
-            type: 'file',
-            filename: path.join(loggerFilePath, "apz_tv.log"),
-            maxLogSize: 1024*1024*50,
-            backups: 2,
-        },
-        apz_guard: {
-            type: 'file',
-            filename: path.join(loggerFilePath, "apz_guard.log"),
-            maxLogSize: 1024*1024*50,
-            backups: 2,
-        },
-        apz_other_users: {
-            type: 'file',
-            filename: path.join(loggerFilePath, "apz_other_users.log"),
-            maxLogSize: 1024*1024*50,
-            backups: 2,
-        },
-        guardlistener: {
-            type: 'file',
-            filename: path.join(loggerFilePath, "guardlistener.log"),
-            maxLogSize: 1024*1024*50,
-            backups: 2,
-        },
-        recorder: {
-            type: 'file',
-            filename: path.join(loggerFilePath, "recorder.log"),
-            maxLogSize: 1024*1024*50,
-            backups: 2,
-        },
         dosign: {
             type: 'file',
             filename: path.join(loggerFilePath, "dosign.log"),
@@ -97,13 +55,6 @@ let config = {
         gold: { appenders: ['console', "gold", "mix", "gift"], level: 'ALL'},
         sliver: { appenders: ['console', "sliver", "mix", "gift"], level: 'ALL'},
 
-        acceptor: { appenders: ['console', "acceptor"], level: 'ALL'},
-        apz_tv: { appenders: ['console', "apz_tv", "apz_default"], level: 'ALL'},
-        apz_guard: { appenders: ['console', "apz_guard", "apz_default"], level: 'ALL'},
-        apz_other_users: { appenders: ['console', "apz_other_users"], level: 'ALL'},
-        guardlistener: { appenders: ['console', "guardlistener"], level: 'ALL'},
-        recorder: { appenders: ['console', "recorder"], level: 'ALL'},
-
         dosign: { appenders: ['console', "dosign"], level: 'ALL'},
         heartbeat: { appenders: ['console', "heartbeat"], level: 'ALL'},
 
@@ -117,14 +68,6 @@ log4js.configure(config);
 module.exports.chat = log4js.getLogger("chat");
 module.exports.gold = log4js.getLogger("gold");
 module.exports.sliver = log4js.getLogger("sliver");
-
-module.exports.apz_tv = log4js.getLogger("apz_tv");
-module.exports.apz_guard = log4js.getLogger("apz_guard");
-module.exports.apz_other_users = log4js.getLogger("apz_other_users");
-
-module.exports.acceptor = log4js.getLogger("acceptor");
-module.exports.guardlistener = log4js.getLogger("guardlistener");
-module.exports.recorder = log4js.getLogger("recorder");
 
 module.exports.dosign = log4js.getLogger("dosign");
 module.exports.heartbeat = log4js.getLogger("heartbeat");
