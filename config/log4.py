c18f0257e280f846ec6e4b3d7329a1d8059ca41c
Caoliang <i@caoliang.net>
2019-02-08 11:53:32 +0800

    use python.

--- config/log4.py
+++ config/log4.py
@@ -9,7 +9,7 @@ log_format = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
 
 error_file_handler = logging.FileHandler(os.path.join(LOG_PATH, "error_stormgift.log"))
 error_file_handler.setFormatter(log_format)
-error_logger = logging.getLogger("error")
+error_logger = logging.getLogger("error_stormgift")
 error_logger.setLevel(logging.ERROR)
 error_logger.addHandler(error_file_handler)
 
@@ -20,7 +20,7 @@ stormgift_file_handler.setFormatter(log_format)
 
 listener_file_handler = logging.FileHandler(os.path.join(LOG_PATH, "listener_stormgift.log"))
 listener_file_handler.setFormatter(log_format)
-listener_logger = logging.getLogger("listener")
+listener_logger = logging.getLogger("listener_stormgift")
 listener_logger.setLevel(logging.DEBUG)
 listener_logger.addHandler(console)
 listener_logger.addHandler(listener_file_handler)
@@ -34,7 +34,7 @@ listener_logger.error = f
 
 acceptor_file_handler = logging.FileHandler(os.path.join(LOG_PATH, "acceptor_stormgift.log"))
 acceptor_file_handler.setFormatter(log_format)
-acceptor_logger = logging.getLogger("acceptor")
+acceptor_logger = logging.getLogger("acceptor_stormgift")
 acceptor_logger.setLevel(logging.DEBUG)
 acceptor_logger.addHandler(console)
 acceptor_logger.addHandler(acceptor_file_handler)
@@ -46,23 +46,9 @@ def f(*args, **kw):
 acceptor_logger.error = f
 
 
-api_file_handler = logging.FileHandler(os.path.join(LOG_PATH, "api_stormgift.log"))
-api_file_handler.setFormatter(log_format)
-api_logger = logging.getLogger("api")
-api_logger.setLevel(logging.DEBUG)
-api_logger.addHandler(console)
-api_logger.addHandler(api_file_handler)
-api_logger.addHandler(stormgift_file_handler)
-_ = api_logger.error
-def f(*args, **kw):
-    error_logger.error(*args, **kw)
-    _(*args, **kw)
-api_logger.error = f
-
-
-status_file_handler = logging.FileHandler(os.path.join(LOG_PATH, "clients_status.log"))
+status_file_handler = logging.FileHandler(os.path.join(LOG_PATH, "status_stormgift.log"))
 status_file_handler.setFormatter(log_format)
-status_logger = logging.getLogger("clients_status")
+status_logger = logging.getLogger("status_stormgift")
 status_logger.setLevel(logging.DEBUG)
 status_logger.addHandler(console)
 status_logger.addHandler(status_file_handler)
@@ -71,5 +57,4 @@ __all__ = (
     "listener_logger",
     "acceptor_logger",
     "status_logger",
-    "api_logger",
 )
