b4b2343d87cdc2e3549db1b94fef0cc6f8044741
Caoliang <i@caoliang.net>
2019-02-21 13:46:13 +0800

    new hansy.

--- xiaoke.py
+++ xiaoke.py
@@ -9,7 +9,7 @@ from utils.biliapi import WsApi, BiliApi
 import logging
 
 from config import config
-LOG_PATH = config["LOG_PATH"]
+LOG_PATH = "./log"  # config["LOG_PATH"]
 
 log_format = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
 console = logging.StreamHandler(sys.stdout)
@@ -25,6 +25,8 @@ logging = logger
 
 
 MONITOR_ROOM_ID = 11472492
+SILVER_GIFT_LIST = []
+ROBOT_ON = True
 
 
 async def load_cookie(index=0):
@@ -71,28 +73,79 @@ async def proc_message(message):
         msg = info[1]
         uid = info[2][0]
         user_name = info[2][1]
+        is_admin = info[2][2]
         ul = info[4][0]
-        dl = info[3][0]
-        deco = info[3][1]
-        logging.info(f"[{uid}] [{user_name}][{ul}] [{deco} {dl}]-> {msg}")
+        d = info[3]
+        dl = d[0] if d else "0"
+        deco = d[1] if d else "^^"
+        logging.info(f"{'[管]' if is_admin else ''}[{uid}] [{user_name}][{ul}] [{deco} {dl}]-> {msg}")
+
+        global ROBOT_ON
+        if ROBOT_ON:
+            flag, cuid, cookie = await load_cookie()
+            if not flag:
+                return
+
+            if is_admin and msg == "关闭聊天":
+                print("聊天关闭")
+                ROBOT_ON = False
+                await BiliApi.send_danmaku("聊天功能已关闭", room_id=MONITOR_ROOM_ID, cookie=cookie)
+                return
+
+            if uid == cuid:
+                return
+
+            flag, msg = await get_tuling_response(msg)
+            if flag:
+                msg = f"{user_name}　{msg}"[:30]
+                await BiliApi.send_danmaku(msg, room_id=MONITOR_ROOM_ID, cookie=cookie)
+        else:
+            if is_admin and msg == "开启聊天":
+                print("聊天开启")
+                ROBOT_ON = True
+                flag, cuid, cookie = await load_cookie()
+                if not flag:
+                    return
+                await BiliApi.send_danmaku("聊天功能已开启", room_id=MONITOR_ROOM_ID, cookie=cookie)
+
+    elif cmd == "SEND_GIFT":
+        data = message.get("data")
+        uid = data.get("uid", "--")
+        face = data.get("face", "")
+        uname = data.get("uname", "")
+        gift_name = data.get("giftName", "")
+        coin_type = data.get("coin_type", "")
+        total_coin = data.get("total_coin", 0)
+        num = data.get("num", "")
+        if coin_type != "gold":
+            SILVER_GIFT_LIST.append(f"{uname}${gift_name}${num}")
+
+    elif cmd == "COMBO_END":
+        data = message.get("data")
+        uname = data.get("uname", "")
+        gift_name = data.get("gift_name", "")
+        price = data.get("price")
+        count = data.get("combo_num", 0)
+        flag, cuid, cookie = await load_cookie()
+        if not flag:
+            return
+        await BiliApi.send_danmaku(f"感谢{uname}赠送的{count}个{gift_name}! 大气大气~", room_id=MONITOR_ROOM_ID, cookie=cookie)
+
+    elif cmd == "GUARD_BUY":
+        data = message.get("data")
+        uid = data.get("uid")
+        uname = data.get("username", "")
+        gift_name = data.get("gift_name", "GUARD")
+        price = data.get("price")
+        num = data.get("num", 0)
 
         flag, cuid, cookie = await load_cookie()
-        if not flag or uid == cuid:
+        if not flag:
             return
-
-        flag, msg = await get_tuling_response(msg)
-        if flag:
-            msg = f"{user_name}　{msg}"[:30]
-            await BiliApi.send_danmaku(msg, room_id=MONITOR_ROOM_ID, cookie=cookie)
+        await BiliApi.send_danmaku(f"感谢{uname}开通了{num}个月的{gift_name}! 大气大气~", room_id=MONITOR_ROOM_ID, cookie=cookie)
 
 
 async def main():
-    async def on_connect():
-        logging.info("Connected!")
-
-    async def on_shut_down(*args):
-        logging.error("dfasfasf")
-
     async def on_connect(ws):
         logging.info("on_connect")
         await ws.send(WsApi.gen_join_room_pkg(MONITOR_ROOM_ID))
@@ -116,9 +169,28 @@ async def main():
     await new_client.start()
     logging.info("Stated")
     while True:
-        await asyncio.sleep(5)
+        gift_list = {}
+        while SILVER_GIFT_LIST:
+            gift = SILVER_GIFT_LIST.pop()
+            uname, gift_name, num = gift.split("$")
+            key = f"{uname}${gift_name}"
+            if key in gift_list:
+                gift_list[key] += int(num)
+            else:
+                gift_list[key] = int(num)
+
+        if gift_list:
+            for key, num in gift_list.items():
+                flag, cuid, cookie = await load_cookie()
+                if not flag:
+                    return
+                uname, gift_name = key.split("$")
+                await BiliApi.send_danmaku(
+                    f"感谢{uname}赠送的{num}个{gift_name}! 大气大气~", room_id=MONITOR_ROOM_ID, cookie=cookie)
+
+        await asyncio.sleep(8)
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.gather(main()))
+loop.run_until_complete(main())
 loop.run_forever()
