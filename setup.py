336ac0e6eee5bbe59bd721c9242c9a98be4d3607
Caoliang <i@caoliang.net>
2019-04-15 12:48:16 +0800

    update setup.py.

--- setup.py
+++ setup.py
@@ -2,14 +2,21 @@
 This script is writen for generation an executable file
 for windows operation system by cxFreeze.
 """
-
+import os
 from cx_Freeze import setup, Executable
 
+os.environ['TCL_LIBRARY'] = r'C:\backup\programfiles\python36\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\backup\programfiles\python36\tcl\tk8.6'
 
 setup(
     name="bilibili_admin_assist",
     version="1.0",
     description="Some description",
+    options={
+        'build_exe': {
+            'packages': ['asyncio']
+        },
+    },
     executables=[
         Executable("run.py")
     ]
