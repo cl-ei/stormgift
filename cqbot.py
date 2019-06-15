42c1d5c0870639db0ae781b43cc126b3cb240bc2
Caoliang <i@caoliang.net>
2019-06-15 19:10:24 +0800

    update cqbot.

--- cqbot.py
+++ cqbot.py
@@ -5,23 +5,23 @@ import json
 import time
 import uuid
 import logging
-import asyncio
 import requests
 import datetime
 import hashlib
 import traceback
-from math import floor
-from threading import Thread
-from random import random, choice
-from utils.ws import ReConnectingWsClient
-
+from random import choice
 from cqhttp import CQHttp
+
+if "linux" in sys.platform:
+    LOG_FILE = "/home/wwwroot/log/cqbot.log"
+else:
+    LOG_FILE = os.path.join("./log", "cqbot.log")
 
 
 log_format = logging.Formatter("%(asctime)s [%(levelname)s]: %(message)s")
 console = logging.StreamHandler(sys.stdout)
 console.setFormatter(log_format)
-log_file_handler = logging.FileHandler(os.path.join("./log", "cqbot.log"), encoding="utf-8")
+log_file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8")
 log_file_handler.setFormatter(log_format)
 logger = logging.getLogger("cqbot")
 logger.setLevel(logging.DEBUG)
@@ -78,167 +78,6 @@ class Settings:
 bot = CQHttp(api_root='http://127.0.0.1:5700/', access_token='123456', secret='654321')
 
 
-class WsApi(object):
-    BILI_WS_URI = "ws://broadcastlv.chat.bilibili.com:2244/sub"
-    PACKAGE_HEADER_LENGTH = 16
-    CONST_MESSAGE = 7
-    CONST_HEART_BEAT = 2
-
-    @classmethod
-    def generate_packet(cls, action, payload=""):
-        payload = payload.encode("utf-8")
-        packet_length = len(payload) + cls.PACKAGE_HEADER_LENGTH
-        buff = bytearray(cls.PACKAGE_HEADER_LENGTH)
-        # package length
-        buff[0] = (packet_length >> 24) & 0xFF
-        buff[1] = (packet_length >> 16) & 0xFF
-        buff[2] = (packet_length >> 8) & 0xFF
-        buff[3] = packet_length & 0xFF
-        # migic & version
-        buff[4] = 0
-        buff[5] = 16
-        buff[6] = 0
-        buff[7] = 1
-        # action
-        buff[8] = 0
-        buff[9] = 0
-        buff[10] = 0
-        buff[11] = action
-        # migic parma
-        buff[12] = 0
-        buff[13] = 0
-        buff[14] = 0
-        buff[15] = 1
-        return bytes(buff + payload)
-
-    @classmethod
-    def gen_heart_beat_pkg(cls):
-        return cls.generate_packet(cls.CONST_HEART_BEAT)
-
-    @classmethod
-    def gen_join_room_pkg(cls, room_id):
-        uid = int(1E15 + floor(2E15 * random()))
-        package = '{"uid":%s,"roomid":%s}' % (uid, room_id)
-        return cls.generate_packet(cls.CONST_MESSAGE, package)
-
-    @classmethod
-    def parse_msg(cls, message):
-        msg_list = []
-        while message:
-            length = (message[0] << 24) + (message[1] << 16) + (message[2] << 8) + message[3]
-            current_msg = message[:length]
-            message = message[length:]
-            if len(current_msg) > 16 and current_msg[16] != 0:
-                try:
-                    msg = current_msg[16:].decode("utf-8", errors="ignore")
-                    msg_list.append(json.loads(msg))
-                except Exception as e:
-                    print("e: %s, m: %s" % (e, current_msg))
-        return msg_list
-
-
-async def __start_ws():
-    def at_all_for_hansy(test=False):
-        url = "https://api.live.bilibili.com/AppRoom/index?platform=android&room_id=2516117"
-        headers = {
-            "Accept": (
-                "text/html,application/xhtml+xml,application/xml;"
-                "q=0.9,image/webp,image/apng,*/*;q=0.8"
-            ),
-            "User-Agent": (
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                "Chrome/70.0.3538.110 Safari/537.36"
-            ),
-        }
-        try:
-            r = requests.get(url=url, headers=headers)
-            if r.status_code != 200:
-                raise Exception("Error status code!")
-            result = json.loads(r.content.decode("utf-8"))
-            title = result.get("data", {}).get("title")
-            image = result.get("data", {}).get("cover")
-        except Exception as e:
-            logging.exception("Error when get live room info: %s" % e, exc_info=True)
-            title = "珩心小姐姐开播啦！快来围观"
-            image = "https://i1.hdslb.com/bfs/archive/a6a3d6f3d3582fd5172f6f829c0fe5522705e399.jpg"
-
-        content = "这里是一只易燃易咆哮的小狮子，宝物是糖果锤！嗷呜(っ*´□`)っ~不关注我的通通都要被一！口！吃！掉！"
-
-        groups = Settings.TEST_GROUP_ID_LIST if test else Settings.NOTICE_GROUP_ID_LIST
-        for group_id in groups:
-            message = "[CQ:share,url=https://live.bilibili.com/2516117,title=%s,content=%s,image=%s]" % (
-                title, content, image
-            )
-            bot.send(context={"message_type": "group", "group_id": group_id}, message=message)
-
-            message = "[CQ:at,qq=all] \n直播啦！！快来听泡泡唱歌咯，本次直播主题：\n%s" % title
-            bot.send(context={"message_type": "group", "group_id": group_id}, message=message)
-
-    async def on_connect(ws):
-        logging.info("connected.")
-        await ws.send(WsApi.gen_join_room_pkg(Settings.LIVE_ROOM_ID))
-
-    async def on_shut_down():
-        logging.error("shutdown!")
-        raise RuntimeError("Connection broken!")
-
-    async def on_message(message):
-
-        for m in WsApi.parse_msg(message):
-            cmd = m.get("cmd")
-            if cmd == "LIVE":
-                Settings.notice(at_all_for_hansy)
-
-            elif cmd == "PREPARING":
-                Settings.prepare()
-                bot.send_private_msg(
-                    user_id=291020256,
-                    message="歌单！\n [CQ:image,file=1.gif]"
-                )
-
-            elif cmd.startswith("DANMU_MSG"):
-                info = m.get("info", {})
-                msg = str(info[1])
-                uid = info[2][0]
-                user_name = info[2][1]
-
-                logging.debug("Danmaku received: %s (%s) -> %s" % (user_name, uid, msg))
-
-                if uid == 20932326:
-                    if msg == "测试通知":
-                        def f():
-                            at_all_for_hansy(test=True)
-
-                        Settings.notice(f)
-
-                    if msg == "重置通知":
-                        Settings.clear_time()
-
-    new_client = ReConnectingWsClient(
-        uri=WsApi.BILI_WS_URI,
-        on_message=on_message,
-        on_connect=on_connect,
-        on_shut_down=on_shut_down,
-        heart_beat_pkg=WsApi.gen_heart_beat_pkg(),
-        heart_beat_interval=10
-    )
-
-    await new_client.start()
-    logging.info("Hansy ws stated.")
-
-    while True:
-        await asyncio.sleep(10)
-
-
-def monitor_danmaku():
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(__start_ws())
-
-
-Thread(target=monitor_danmaku, daemon=True).start()
-
-
 class BotUtils:
 
     @classmethod
@@ -247,7 +86,7 @@ class BotUtils:
         try:
             r = requests.get(url)
             assert r.status_code == 200
-            file = f"C:\\backup\\programfiles\\酷Q Pro\\data\\record\\{word}.mp3"
+            file = f"{word}.mp3"
             with open(file, "wb") as f:
                 f.write(r.content)
 
@@ -682,4 +521,4 @@ def handle_meta_event(context):
     return
 
 
-bot.run(host='127.0.0.1', port=8080)
+bot.run(host='127.0.0.1', port=60000)
