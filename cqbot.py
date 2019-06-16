1a3c148c164c9af6a6fbfd98bc5aeb5d0a62c4d1
Caoliang <i@caoliang.net>
2019-06-16 12:11:12 +0800

    update cqbot.

--- cqbot.py
+++ cqbot.py
@@ -432,7 +432,7 @@ def handle_msg(context):
         if user_id == 80873436 and msg.startswith("转发"):
             msg = msg[2:]
             relay_user_id, raw_msg = msg.split("-", 1)
-            bot.send_private_msg(user_id=relay_user_id, message=raw_msg)
+            bot.send_private_msg(user_id=int(relay_user_id), message=raw_msg)
 
         elif msg.startswith("起床"):
             try:
