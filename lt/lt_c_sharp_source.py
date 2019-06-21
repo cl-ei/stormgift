2ec005b846ebcb713abc349c5fbf5dbba139d86d
Caoliang <i@caoliang.net>
2019-06-21 18:47:08 +0800

    new ws client and tv source.

--- lt/lt_c_sharp_source.py
+++ lt/lt_c_sharp_source.py
@@ -1,31 +1,37 @@
 import ssl
+import rsa
+import sys
 import json
 import socket
 
-rsa = """MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCmzmmY8qjMY9/izLKRvGO/kVqWys/S5y39Ly6LsYJNAMrYPoG1YlCWiuRT3Fh/YpJeDo+XsO2Rx+Bussl6XoPPr1RGn1kVFyfM8Q8INCMhnW3NaM6P8IEnBnr+WBb8RTNxuVrnVeSMomPGCKqDavVQ9jCV8ih9y5W3gfrWMYBsqQIDAQAB"""
+
+with open("./key.txt", "r") as f:
+    pubkey = rsa.PublicKey.load_pkcs1_openssl_pem(f.read().encode())
+s = rsa.encrypt("ExHelper".encode("utf-8"), pubkey)
+print(str(s))
 
 
 def client(host, port, cafile=None):
     context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    hands_shake = json.dumps(
+        {
+            "cmd": "Auth",
+            "data": {
+                "rsa": str(s)
+            }
+        }
+    ).encode("utf-8")
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock = context.wrap_socket(sock, server_hostname=host)
     sock.connect((host, port))
 
-    hands_shake = json.dumps(
-        {
-            "cmd": "Auth",
-            "data": {
-                "rsa": rsa
-            }
-        }
-    ).encode("utf-8")
-    hands_shake_len = len(hands_shake)
+    hands_shake_len = len(hands_shake) + 4
     length = bytearray(4)
-    length[0] = hands_shake_len & 0xFF
-    length[1] = (hands_shake_len >> 8) & 0xFF
-    length[2] = (hands_shake_len >> 16) & 0xFF
-    length[3] = (hands_shake_len >> 24) & 0xFF
+    length[3] = hands_shake_len & 0xFF
+    length[2] = (hands_shake_len >> 8) & 0xFF
+    length[1] = (hands_shake_len >> 16) & 0xFF
+    length[0] = (hands_shake_len >> 24) & 0xFF
 
     r = sock.send(length + hands_shake)
     print(f"send r: {r}")
