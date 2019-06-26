3f482213f580258ae9aa78ecd53cd81c9fa82819
Caoliang <i@caoliang.net>
2019-06-26 12:55:53 +0800

    update searcher.

--- lt/lt_search_valuable_live_room.py
+++ lt/lt_search_valuable_live_room.py
@@ -1,25 +1,62 @@
 import asyncio
+import datetime
 from utils.biliapi import BiliApi
-from utils.dao import ValuableLiveRoom
+from utils.model import LiveRoomInfo, objects
 
 BiliApi.USE_ASYNC_REQUEST_METHOD = True
 
 
 async def search_short_number():
     print("search_short_number...")
+
     for room_id in range(1, 999):
-        await asyncio.sleep(2)
+        live_room_info = {}
 
         req_url = F"https://api.live.bilibili.com/AppRoom/index?room_id={room_id}&platform=android"
         flag, data = await BiliApi.get(req_url, timeout=10, check_error_code=True)
-        if flag:
+        if flag and data["code"] in (0, "0"):
             short_room_id = data["data"]["show_room_id"]
             real_room_id = data["data"]["room_id"]
-            r = await ValuableLiveRoom.add((short_room_id, real_room_id))
-            print(f"Live room add: {short_room_id}->{real_room_id} -> {r}")
-        else:
-            if "not exists" in data:
-                continue
-            else:
-                print(f"Room id: {room_id}, msg: {data}")
+            user_id = data["data"]["mid"]
 
+            live_room_info["short_room_id"] = short_room_id
+            live_room_info["real_room_id"] = real_room_id
+            live_room_info["title"] = ""  # data["data"]["title"].encode("utf-8")
+            live_room_info["user_id"] = user_id
+            live_room_info["create_at"] = data["data"]["create_at"]
+            live_room_info["attention"] = data["data"]["attention"]
+
+            req_url = f"https://api.live.bilibili.com/guard/topList?roomid={real_room_id}&page=1&ruid={user_id}"
+            flag, data = await BiliApi.get(req_url, timeout=10, check_error_code=True)
+            if not flag:
+                print(f"Error! e: {data}")
+                continue
+
+            live_room_info["guard_count"] = data["data"]["info"]["num"]
+            print(f"{live_room_info}")
+
+            flag, r = await LiveRoomInfo.update_live_room(**live_room_info)
+            print(
+                f"Update {'success' if flag else 'Failed'}! room_id: "
+                f"{live_room_info['short_room_id']}->{live_room_info['real_room_id']}, r: {r}"
+            )
+        await asyncio.sleep(10)
+
+
+async def search_normal():
+    pass
+
+
+async def main():
+    await objects.connect()
+
+    while True:
+        now_hour = datetime.datetime.now().hour
+        if 2 < now_hour < 13:
+            await search_short_number()
+            await search_normal()
+
+        await asyncio.sleep(60)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
---
+++ utils/model.py
@@ -0,0 +1,60 @@
+import peewee
+import asyncio
+import datetime
+
+from peewee_async import Manager, PooledMySQLDatabase
+from config import MYSQL_CONFIG
+
+mysql_db = PooledMySQLDatabase(**MYSQL_CONFIG)
+
+loop = asyncio.get_event_loop()
+objects = Manager(mysql_db, loop=loop)
+
+
+class LiveRoomInfo(peewee.Model):
+    short_room_id = peewee.IntegerField()
+    real_room_id = peewee.IntegerField(null=False, unique=True)
+    title = peewee.CharField()
+    user_id = peewee.IntegerField()
+    create_at = peewee.CharField()
+
+    attention = peewee.IntegerField()
+    guard_count = peewee.IntegerField()
+
+    update_time = peewee.DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mysql_db
+
+    @classmethod
+    async def update_live_room(cls, short_room_id, real_room_id, title, user_id, create_at, attention, guard_count):
+        try:
+            live_rooms = await objects.execute(LiveRoomInfo.select().where(LiveRoomInfo.real_room_id == real_room_id))
+            if live_rooms:
+                live_room_obj = live_rooms[0]
+
+                live_room_obj.short_room_id = short_room_id
+                live_room_obj.real_room_id = real_room_id
+                live_room_obj.title = title
+                live_room_obj.user_id = user_id
+                live_room_obj.create_at = create_at
+                live_room_obj.attention = attention
+                live_room_obj.guard_count = guard_count
+                live_room_obj.update_time = datetime.datetime.now()
+                await objects.update(live_room_obj)
+            else:
+                live_room_obj = await objects.create(
+                    LiveRoomInfo,
+                    short_room_id=short_room_id,
+                    real_room_id=real_room_id,
+                    title=title,
+                    user_id=user_id,
+                    create_at=create_at,
+                    attention=attention,
+                    guard_count=guard_count,
+                    update_time=datetime.datetime.now()
+                )
+            return True, live_room_obj
+        except Exception as e:
+            import traceback
+            return False, f"Error: {e}, {traceback.format_exc()}"
