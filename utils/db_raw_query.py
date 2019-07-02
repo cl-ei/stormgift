2eeaafc751f3d8e90fe807250146c23682c3bd7d
Caoliang <i@caoliang.net>
2019-07-02 12:25:16 +0800

    new commands in dd live room.

--- utils/db_raw_query.py
+++ utils/db_raw_query.py
@@ -5,21 +5,21 @@ from config import MYSQL_CONFIG
 
 class AsyncMySQL:
 
-    conn = None
-
-    @classmethod
-    async def execute(cls, *args, **kwargs):
-        if cls.conn is None:
-            cls.conn = await aiomysql.connect(
-                host=MYSQL_CONFIG["host"],
-                port=MYSQL_CONFIG["port"],
-                user=MYSQL_CONFIG["user"],
-                password=MYSQL_CONFIG["password"],
-                db=MYSQL_CONFIG["database"],
-                loop=asyncio.get_event_loop()
-            )
-        cursor = await cls.conn.cursor()
+    @staticmethod
+    async def execute(*args, **kwargs):
+        conn = await aiomysql.connect(
+            host=MYSQL_CONFIG["host"],
+            port=MYSQL_CONFIG["port"],
+            user=MYSQL_CONFIG["user"],
+            password=MYSQL_CONFIG["password"],
+            db=MYSQL_CONFIG["database"],
+            loop=asyncio.get_event_loop()
+        )
+        cursor = await conn.cursor()
         await cursor.execute(*args, **kwargs)
         r = await cursor.fetchall()
+
         await cursor.close()
+        conn.close()
+
         return r
