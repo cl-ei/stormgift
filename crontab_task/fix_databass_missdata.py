79a008c8b2a183d5bab34750805d42d4d6dc4fb3
Caoliang <i@caoliang.net>
2019-07-05 22:53:03 +0800

    fix missed data.

--- crontab_task/fix_databass_missdata.py
+++ crontab_task/fix_databass_missdata.py
@@ -1,5 +1,5 @@
 import asyncio
-from utils.reconstruction_model import Raffle, BiliUser, objects
+from utils.reconstruction_model import Raffle, BiliUser, Guard, objects
 from utils.db_raw_query import AsyncMySQL
 from utils.highlevel_api import ReqFreLimitApi
 from config.log4 import console_logger as logging
@@ -9,6 +9,7 @@ async def fix_user_record_missed_uid():
     await objects.connect()
 
     non_uid_users = await objects.execute(BiliUser.select().where(BiliUser.uid == None))
+    logging.info(f"non_uid_users count: {len(non_uid_users)}")
 
     for non_uid_user_obj in non_uid_users:
         await asyncio.sleep(3)
@@ -32,7 +33,7 @@ async def fix_user_record_missed_uid():
             # 有两个user_obj
             # 1.先把旧的existed_user_obj的name更新
             has_uid_user_obj.name = lastest_user_name
-            await objects.update(has_uid_user_obj, only="name")
+            await objects.update(has_uid_user_obj, only=("name", ))
 
             # 2.迁移所有的raffle记录
             updated_count = 0
@@ -41,23 +42,31 @@ async def fix_user_record_missed_uid():
             ):
                 updated_count += 1
                 raffle_obj.sender_obj_id = has_uid_user_obj.id
-                await objects.update(raffle_obj, "sender_obj_id")
+                await objects.update(raffle_obj, only=("sender_obj_id", ))
 
             for raffle_obj in await objects.execute(
                     Raffle.select().where(Raffle.winner_obj_id == non_uid_user_obj.id)
             ):
                 updated_count += 1
                 raffle_obj.winner_obj_id = has_uid_user_obj.id
-                await objects.update(raffle_obj, "winner_obj_id")
+                await objects.update(raffle_obj, only=("winner_obj_id", ))
 
-            # 3.删除空的user_obj #还有guard数据，所以不能删除
-            # await objects.delete(non_uid_user_obj)
+            # 3 迁移guard记录
+            for guard_obj in await objects.execute(
+                Guard.select().where(Guard.sender_obj_id == non_uid_user_obj.id)
+            ):
+                updated_count += 1
+                guard_obj.sender_obj_id = has_uid_user_obj.id
+                await objects.update(guard_obj, only=("sender_obj_id", ))
+
+            # 4.删除空的user_obj
+            await objects.delete(non_uid_user_obj)
 
             logging.info(f"User obj updated! raffle update count: {updated_count}")
 
         else:
             non_uid_user_obj.uid = uid
-            await objects.update(non_uid_user_obj, only="uid")
+            await objects.update(non_uid_user_obj, only=("uid", ))
             logging.info(f"User obj updated! {lastest_user_name} -> {uid}, obj id: {non_uid_user_obj.id}")
 
     await objects.close()
