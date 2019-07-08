0e730c0afdbb9831fdaffbabcbcaf3aa833d4b4e
Caoliang <i@caoliang.net>
2019-07-08 10:28:31 +0800

    update cq bot.

--- cq_bot_async.py
+++ cq_bot_async.py
@@ -555,8 +555,7 @@ class BotHandler:
 
 
 async def handler(request):
-    text = await request.text()
-    print(f"text: {text}")
+
     context = await request.json()
     if context["post_type"] == "message":
         response = await BotHandler.handle_message(context)
