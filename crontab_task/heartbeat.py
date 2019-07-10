56b5f5fd93c8c415368880fb16b0753ce7783e2f
Caoliang <i@caoliang.net>
2019-07-10 19:29:45 +0800

    fix dd.

--- crontab_task/heartbeat.py
+++ crontab_task/heartbeat.py
@@ -3,8 +3,9 @@ from utils.highlevel_api import DBCookieOperator
 from config.log4 import crontab_task_logger as logging
 
 
-async def post_heartbeat(cookie):
-    logging.info(f"Post heartbeat for {cookie.split(';')[0]}.")
+async def post_heartbeat(lt_user_obj):
+    cookie = lt_user_obj.cookie
+    logging.info(f"Post heartbeat for {lt_user_obj.name}(uid: {lt_user_obj.DedeUserID}).")
 
     from utils.biliapi import BiliApi
     r, data = await BiliApi.post_heartbeat_5m(cookie)
@@ -22,7 +23,7 @@ async def post_heartbeat(cookie):
 async def main():
     objs = await DBCookieOperator.get_objs(available=True, is_vip=True)
     for obj in objs:
-        await post_heartbeat(obj.cookie)
+        await post_heartbeat(obj)
         await asyncio.sleep(5)
     logging.info("Post heart beat task done.\n\n")
 
