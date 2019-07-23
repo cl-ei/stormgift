99c7cf3386041174f38fe9f68162a4553e781c1f
Caoliang <i@caoliang.net>
2019-07-23 10:10:12 +0800

    update raffle recorder.

--- manual_scripts/fix_raffle.sql
+++ manual_scripts/fix_raffle.sql
@@ -4,4 +4,5 @@ update raffle set gift_name = "小电视飞船抽奖" where gift_name = "-" and 
 update raffle set gift_name = "任意门抽奖" where gift_name = "-" and gift_type = "GIFT_30035";
 update raffle set gift_name = "幻乐之声抽奖" where gift_name = "-" and gift_type = "GIFT_30207";
 update raffle set gift_name = "摩天大楼抽奖" where gift_name = "-" and gift_type = "GIFT_20003";
+update raffle set gift_name = "金腰带抽奖" where gift_name = "-" and gift_type = "GIFT_30266";
 
