ffa8462c7ed935b568c26e305bafde79bb94e7d1
Caoliang <i@caoliang.net>
2019-09-23 12:03:07 +0800

    update lt acceptor.

--- crontab_task/do_sign.py
+++ crontab_task/do_sign.py
@@ -6,18 +6,25 @@ from config.log4 import crontab_task_logger as logging
 
 
 async def main():
-    logging.info(f"Start do sign task.")
+    start_prompt = f"Start do sign task"
+    split_char_len = (100 - len(start_prompt)) // 2
+    split_char = f"{'-' * split_char_len}"
+    logging_msg_list = [
+        f"{split_char}{start_prompt}{split_char}\n",
+    ]
     start_time = time.time()
     objs = await DBCookieOperator.get_objs(available=True)
 
     for obj in objs:
-
-        logging.info(f"Now proc {obj.name}(uid: {obj.DedeUserID}).")
+        logging_msg_list.append(f"\nNow proc {obj.name}(uid: {obj.DedeUserID}).\n")
 
         cookie = obj.cookie
         flag, result = await BiliApi.do_sign(cookie)
         if not flag and "请先登录" in result:
-            logging.warning(f"Do sign failed, user: {obj.name} - {obj.DedeUserID} flag: {flag}, result: {result}")
+            logging_msg_list.append(
+                f"WARNING: Do sign failed, user: {obj.name} - {obj.DedeUserID}, "
+                f"flag: {flag}, result: {result}\n"
+            )
             await DBCookieOperator.set_invalid(obj)
             continue
 
@@ -28,13 +35,16 @@ async def main():
             if is_vip != obj.is_vip:
                 await DBCookieOperator.set_vip(obj, is_vip)
         else:
-            logging.warning(f"Get if it is vip failed, user: {obj.name} - {obj.DedeUserID} flag: {flag}, is_vip: {is_vip}")
+            logging_msg_list.append(
+                f"WARNING: Get if it is vip failed, user: {obj.name} - {obj.DedeUserID}, "
+                f"flag: {flag}, is_vip: {is_vip}\n"
+            )
 
         await asyncio.sleep(0.5)
 
         r, data = await BiliApi.do_sign_group(cookie)
         if not r:
-            logging.error(f"Sign group failed, {obj.name}-{obj.DedeUserID}: {data}")
+            logging_msg_list.append(f"ERROR: Sign group failed, {obj.name}-{obj.DedeUserID}: {data}\n")
 
         await asyncio.sleep(0.5)
         await BiliApi.do_sign_double_watch(cookie)
@@ -45,8 +55,9 @@ async def main():
 
         # 触发领取今日辣条
         await BiliApi.get_bag_list(cookie=cookie)
-
-    logging.info(f"Do sign task done. cost: {int((time.time() - start_time) *1000)} ms.\n\n")
+    logging_msg_list.append(f"\nDo sign task done. cost: {int((time.time() - start_time) *1000)} ms.\n")
+    logging_msg_list.append(f"{'-'*100}")
+    logging.info("".join(logging_msg_list))
 
 
 loop = asyncio.get_event_loop()
