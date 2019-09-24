5119ec7aa1ecd3c72c2cb17b593d62e482e104d9
Caoliang <i@caoliang.net>
2019-09-24 14:58:53 +0800

    update lt acceptor.

--- utils/highlevel_api.py
+++ utils/highlevel_api.py
@@ -470,11 +470,9 @@ class DBCookieOperator:
         total_intimacy = sum([r[2] for r in raffle_result if r[0] != "宝箱"])
         postfix = []
         for r in sorted(raffle_result, key=sort_func):
-            postfix.append("-" * 20 + "\n")
             gift_name = r[0]
             award_name = "银瓜子" if gift_name == "宝箱" else "辣条"
-            postfix.append(f"{gift_name}: {r[1]}次, {r[2]}{award_name}\n")
-        postfix.append("-" * 20)
+            postfix.append(f"{gift_name}: {r[1]}次、{r[2]}{award_name}, ")
 
         if (datetime.datetime.now() - cookie_obj.blocked_time).total_seconds() < 3600 * 6:
             blocked_datetime = cookie_obj.blocked_time
