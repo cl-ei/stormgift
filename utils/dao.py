f517fd7198f570f2fe08c135a2d2b8a7d96354eb
Caoliang <i@caoliang.net>
2019-09-28 22:26:57 +0800

    update website.

--- utils/dao.py
+++ utils/dao.py
@@ -552,37 +552,26 @@ class LTUserSettings:
 
 
 class UserRaffleRecordBasedOnRedis:
-    gift_name_to_intimacy_key = "LT_GIFT_NAME_TO_INTIMACY"
-    user_raffle_record_key = "LT_USER_RAFFLE"
+    user_raffle_record_key = "LT_USER_RAFFLE_CNT"
 
     @classmethod
-    async def record(cls, user_id, gift_name, raffle_id, intimacy=0, created_time=None):
-        gift_name_to_intimacy_key = f"{cls.gift_name_to_intimacy_key}_{gift_name}"
-        await redis_cache.set(gift_name_to_intimacy_key, intimacy)
-
-        if created_time is None:
-            created_time = int(time.time())
-
-        user_raffle_record_key = f"{cls.user_raffle_record_key}_{user_id}_{datetime.datetime.now().date()}"
-        value = f"{raffle_id}_{created_time}"
-        await redis_cache.list_push(user_raffle_record_key, value)
+    async def record(cls, user_id, gift_name, raffle_id, intimacy=0):
+        user_raffle_record_key = (
+            f"{cls.user_raffle_record_key}_{user_id}_"
+            f"{datetime.datetime.now().date()}_{gift_name}_{intimacy}"
+        )
+        await redis_cache.incr(user_raffle_record_key)
         await redis_cache.expire(user_raffle_record_key, timeout=3600*72)
 
     @classmethod
     async def get_24(cls, user_id):
-        """
-
-        :param user_id:
-        :return: last_time,
-        """
-        gift_names_key = f"{cls.gift_name_to_intimacy_key}_*"
-        await redis_cache.keys(gift_names_key)
+        pass
 
 
 async def test():
-    r = await LTUserSettings.get(20932326)
+    r = await UserRaffleRecordBasedOnRedis.get_24(20932326)
     print(r)
-    # await HansyDynamicNotic.remove(i)
+
     pass
 
 
