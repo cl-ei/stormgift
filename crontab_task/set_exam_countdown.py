8a0401e80b93bb955bf3c5a7ee1c2ff354b2d7ec
Caoliang <i@caoliang.net>
2019-10-23 14:34:53 +0800

    add countdown.

--- crontab_task/set_exam_countdown.py
+++ crontab_task/set_exam_countdown.py
@@ -6,7 +6,7 @@ def main():
     end_date = datetime.datetime.strptime("2019-12-20 00:00:00", "%Y-%m-%d %H:%M:%S")
     count_down = (end_date - datetime.datetime.now()).total_seconds()
     days = int(count_down / 3600 / 24)
-    card = f"考研倒计时{days}天"
+    card = f"梓亞.考研倒计时{days}天"
     r = bot_zy.set_group_card(group_id=159855203, user_id=250666570, card=card)
     print(f"Execute result: {r}, card: {card}")
 
