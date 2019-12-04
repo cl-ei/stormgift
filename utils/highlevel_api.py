7aa91e750cbab0de8972501519892d66970c4838
Caoliang <i@caoliang.net>
2019-12-04 13:53:25 +0800

    update ws.

--- utils/highlevel_api.py
+++ utils/highlevel_api.py
@@ -495,7 +495,9 @@ class DBCookieOperator:
             return False, f"{uid}未登录宝藏站点。"
 
         def gen_time_prompt(interval):
-            if interval > 3600*24:
+            if interval > 3600*24*365:
+                return f"很久以前"
+            elif interval > 3600*24:
                 return f"约{int(interval // (3600*24))}天前"
             elif interval > 3600:
                 return f"约{int(interval // 3600)}小时前"
