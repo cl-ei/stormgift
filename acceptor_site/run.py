84dbfed8a60fb66aa6aa81bfc431a9722dd6f825
Caoliang <i@caoliang.net>
2019-12-24 10:01:59 +0800

    REBUILD PROJ. ADD NEW NODE.

--- acceptor_site/run.py
+++ acceptor_site/run.py
@@ -40,7 +40,7 @@ async def main():
     runner = web.AppRunner(app)
     await runner.setup()
 
-    site = web.TCPSite(runner, '0.0.0.0', 80)
+    site = web.TCPSite(runner, '0.0.0.0', 1024)
     await site.start()
     print("Site started.")
 
