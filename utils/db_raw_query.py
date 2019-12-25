2062e5d9c506aa790875ab1f91a2818d9a50f6aa
Caoliang <i@caoliang.net>
2019-12-25 23:07:52 +0800

    update async mysql.

--- utils/db_raw_query.py
+++ utils/db_raw_query.py
@@ -2,24 +2,33 @@ import asyncio
 import aiomysql
 from config import MYSQL_CONFIG
 
+db_pool = None
+
 
 class AsyncMySQL:
-
     @classmethod
     async def execute(cls, *args, _commit=False, **kwargs):
-        conn = await aiomysql.connect(
-            host=MYSQL_CONFIG["host"],
-            port=MYSQL_CONFIG["port"],
-            user=MYSQL_CONFIG["user"],
-            password=MYSQL_CONFIG["password"],
-            db=MYSQL_CONFIG["database"],
-            loop=asyncio.get_event_loop()
-        )
+        global db_pool
+        if db_pool is None:
+            db_pool = await aiomysql.create_pool(
+                host=MYSQL_CONFIG["host"],
+                port=MYSQL_CONFIG["port"],
+                user=MYSQL_CONFIG["user"],
+                password=MYSQL_CONFIG["password"],
+                db=MYSQL_CONFIG["database"],
+                loop=asyncio.get_event_loop()
+            )
 
-        async with conn.cursor() as cursor:
-            await cursor.execute(*args, **kwargs)
-            if _commit:
-                await conn.commit()
-            r = await cursor.fetchall()
-        conn.close()
-        return r
+        async with db_pool.acquire() as conn:
+            async with conn.cursor() as cursor:
+                await cursor.execute(*args, **kwargs)
+                if _commit:
+                    await conn.commit()
+                return await cursor.fetchall()
+
+    @classmethod
+    async def close(cls):
+        global db_pool
+        db_pool.close()
+        await db_pool.wait_closed()
+        db_pool = None
