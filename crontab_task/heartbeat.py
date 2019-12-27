44e51dfd8b21a49106ecbd60318e9182ceddd9cb
Caoliang <i@caoliang.net>
2019-12-28 01:12:04 +0800

    UPDATE RAFFLE!

--- crontab_task/heartbeat.py
+++ crontab_task/heartbeat.py
@@ -8,17 +8,17 @@ async def post_heartbeat(index, total, lt_user_obj):
     cookie = lt_user_obj.cookie
     r, data = await BiliApi.post_heartbeat_5m(cookie)
     if not r:
-        logging.error(f"Post heartbeat failed! msg: {data}")
+        logging.error(f"post_heartbeat_5m failed! msg: {data}")
         return
 
     r, data = await BiliApi.post_heartbeat_last_timest(cookie)
     if not r:
-        logging.error(f"Cannot post last time st! msg: {data}")
+        logging.error(f"post_heartbeat_last_timest failed! msg: {data}")
         return
 
     r, data = await BiliApi.post_heartbeat_app(cookie=cookie, access_token=lt_user_obj.access_token)
     if not r:
-        logging.error(f"Cannot post last time st! msg: {data}")
+        logging.error(f"post_heartbeat_app failed! msg: {data}")
         return
     logging.info(f"({index + 1}/{total}) Post heartbeat for {lt_user_obj.name}(uid: {lt_user_obj.DedeUserID}) done.")
 
