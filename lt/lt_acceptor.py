e56169d4275d1de4ca72de5861b4108cdc783dea
Caoliang <i@caoliang.net>
2019-12-28 23:13:03 +0800

    update acceptor.

--- lt/lt_acceptor.py
+++ lt/lt_acceptor.py
@@ -1,4 +1,3 @@
-import sys
 import time
 import json
 import random
@@ -6,7 +5,6 @@ import asyncio
 import aiohttp
 import datetime
 import traceback
-from utils.ws import RCWebSocketClient
 from config import cloud_acceptors
 from utils.highlevel_api import DBCookieOperator
 from utils.dao import DelayAcceptGiftsQueue
@@ -27,172 +25,131 @@ NON_SKIP_USER_ID = [
 delay_accept_q = asyncio.Queue()
 
 
-async def async_post(url, data=None, json=None, headers=None, timeout=30):
-    client_session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=timeout))
+async def cloud_accept(act, room_id, gift_id, cookies, gift_type, url):
+    """
+
+    RETURN: flag -> bool, result -> list
+    """
+    req_json = {
+        "act": act,
+        "room_id": room_id,
+        "gift_id": gift_id,
+        "cookies": [c.cookie for c in cookies],
+        "gift_type": gift_type,
+    }
+    client_session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=30))
     try:
         async with client_session as session:
-            async with session.post(url, data=data, json=json, headers=headers) as resp:
+            async with session.post(url, json=req_json) as resp:
                 status_code = resp.status
+                if status_code != 200:
+                    return False, "Cloud Function Error!"
                 content = await resp.text()
-                return status_code, content
-
+                return True, json.loads(content)
     except Exception as e:
-        return 5000, f"Error in request: {e}"
+        return False, f"Cloud Function Connection Error: {e}"
 
 
-class Worker(object):
-    def __init__(self, index, q):
-        self.worker_index = index
-        self.q = q
-        self.__busy_time = 0
+async def accept(index, act, room_id, gift_id, gift_type, gift_name):
+    cookie_objs = await DBCookieOperator.get_objs(available=True, non_blocked=True)
+    # temporary_blocked = await LTTempBlack.get_blocked()
+    # cookie_objs = [c for c in cookie_objs if c.uid not in temporary_blocked]
 
-        self._cookie_objs = []
-        self._cookie_objs_update_time = 0
+    filter_k = {
+        "join_tv_v5": "tv_percent",
+        "join_guard": "guard_percent",
+        "join_pk": "pk_percent",
+    }[act]
 
-    async def load_cookie(self):
-        if time.time() - self._cookie_objs_update_time > 100:
-            self._cookie_objs = await DBCookieOperator.get_objs(available=True, non_blocked=True)
-            self._cookie_objs_update_time = time.time()
+    user_cookie_objs = await LTUserSettings.filter_cookie(cookie_objs, key=filter_k)
+    if not user_cookie_objs:
+        return
 
-        return self._cookie_objs
+    cloud_acceptor_url = random.choice(cloud_acceptors)
+    flag, result = await cloud_accept(
+        act=act,
+        room_id=room_id,
+        gift_id=gift_id,
+        cookies=[c.cookie for c in user_cookie_objs],
+        gift_type=gift_type,
+        url=cloud_acceptor_url,
+    )
+    if not flag:
+        return logging.error(f"Accept Failed! e: {result}")
 
-    async def proc_single(self, data):
-        raffle_type = data.get("raffle_type")
-        room_id = data.get("real_room_id")
-        gift_id = data.get("raffle_id")
-        gift_type = data.get("gift_type", "")
-        gift_name = data.get("gift_name", "未知")
+    success = []
+    success_uid_list = []
+    failed = []
+    for index, cookie_obj in enumerate(user_cookie_objs):
+        flag, message = result[index]
 
-        cookie_objs = await self.load_cookie()
-        # temporary_blocked = await LTTempBlack.get_blocked()
-        # cookie_objs = [c for c in cookie_objs if c.uid not in temporary_blocked]
+        if flag is not True:
+            if "访问被拒绝" in message:
+                await DBCookieOperator.set_blocked(cookie_obj)
+            elif "请先登录哦" in message:
+                await DBCookieOperator.set_invalid(cookie_obj)
+            # elif "你已经领取过" in message or "您已参加抽奖" in message:
+            #     await LTTempBlack.manual_accept_once(uid=cookie_obj.uid)
 
-        if raffle_type == "tv":
-            act, filter_k = "join_tv_v5", "tv_percent"
-        elif raffle_type == "guard":
-            act, filter_k = "join_guard", "guard_percent"
-        elif raffle_type == "pk":
-            act, filter_k = "join_pk", "pk_percent"
-        elif raffle_type == "anchor":
-            act, filter_k = "join_anchor", "anchor_percent"
-        else:
-            return
+            if index != 0:
+                message = message[:100]
+            failed.append(f"{cookie_obj.name}({cookie_obj.uid}){message}")
+            continue
 
-        user_cookie_objs = await LTUserSettings.filter_cookie(cookie_objs, key=filter_k)
-        if not user_cookie_objs:
-            return
+        try:
+            award_num, award_name = message.split("_", 1)
+            award_num = int(award_num)
+            if award_name in ("辣条", "亲密度"):
+                pass
+            elif award_name in ("银瓜子", "金瓜子"):
+                award_num //= 100
+            else:
+                award_num = 0
+        except Exception as e:
+            logging.error(f"Cannot fetch award_num from message. {e}", exc_info=True)
+            award_num = 1
 
-        req_json = {
-            "act": act,
-            "room_id": room_id,
-            "gift_id": gift_id,
-            "cookies": [c.cookie for c in user_cookie_objs],
-            "gift_type": gift_type,
-        }
-        cloud_acceptor_url = random.choice(cloud_acceptors)
-        status_code, content = await async_post(url=cloud_acceptor_url, json=req_json)
-        if status_code != 200:
-            return logging.error(f"Accept Failed! e: {content}")
+        await UserRaffleRecord.create(cookie_obj.uid, gift_name, gift_id, intimacy=award_num)
+        success.append(f"{cookie_obj.name}({cookie_obj.uid})")
+        success_uid_list.append(cookie_obj.uid)
 
-        result_list = json.loads(content)
+    await LTLastAcceptTime.update(*success_uid_list)
 
-        success = []
-        success_uid_list = []
-        failed = []
-        for index, cookie_obj in enumerate(user_cookie_objs):
-            flag, message = result_list[index]
-
-            if flag is not True:
-                if "访问被拒绝" in message:
-                    await DBCookieOperator.set_blocked(cookie_obj)
-                    self._cookie_objs_update_time = 0
-                elif "请先登录哦" in message:
-                    await DBCookieOperator.set_invalid(cookie_obj)
-                    self._cookie_objs_update_time = 0
-                elif act == "join_storm" and "验证码没通过" in message:
-                    logging.warning(f"{cookie_obj.name}(uid: {cookie_obj.uid}) {message}. set blocked.")
-                    await StormGiftBlackRoom.set_blocked(cookie_obj.uid)
-                elif "你已经领取过" in message or "您已参加抽奖" in message:
-                    await LTTempBlack.manual_accept_once(uid=cookie_obj.uid)
-
-                if index != 0:
-                    message = message[:100]
-                failed.append(f"{cookie_obj.name}({cookie_obj.uid}){message}")
-                continue
-
-            try:
-                award_num, award_name = message.split("_", 1)
-                award_num = int(award_num)
-                if award_name in ("辣条", "亲密度"):
-                    pass
-                elif award_name in ("银瓜子", "金瓜子"):
-                    award_num //= 100
-                else:
-                    award_num = 0
-
-            except Exception as e:
-                logging.error(f"Cannot fetch award_num from message. {e}", exc_info=True)
-                award_num = 1
-
-            await UserRaffleRecord.create(cookie_obj.uid, gift_name, gift_id, intimacy=award_num)
-            success.append(f"{cookie_obj.name}({cookie_obj.uid})")
-            success_uid_list.append(cookie_obj.uid)
-
-        await LTLastAcceptTime.update(*success_uid_list)
-
-        success_users = []
-        for i, s in enumerate(success):
-            success_users.append(s)
-            if i > 0 and i % 4 == 0:
-                success_users.append("\n")
-        success_users = "".join(success_users)
-        failed_prompt = f"{'-'*20} FAILED {'-'*20}\n{'^'.join(failed)}\n" if failed else ""
-        title = f"{act.upper()} OK {gift_name} @{room_id}${gift_id}"
-        split_char_count = max(0, (80 - len(title)) // 2)
-        logging.info(
-            f"\n{'-'*split_char_count}{title}{'-'*split_char_count}\n"
-            f"{success_users}\n"
-            f"{failed_prompt}"
-            f"\nWorker: {self.worker_index}, cloud_acceptor: {cloud_acceptor_url[-20:]}, total: {len(success)}\n"
-            f"{'-'*80}"
-        )
-
-    async def accept_delayed(self):
-        while True:
-            data = await self.q.get()
-            start_time = time.time()
-            try:
-                r = await self.proc_single(data)
-            except Exception as e:
-                logging.error(f"DELAY Acceptor {self.worker_index}. error: {e}, {traceback.format_exc()}")
-                continue
-
-            cost_time = time.time() - start_time
-            if cost_time > 5:
-                logging.info(f"DELAY Acceptor {self.worker_index} success, r: {r}, cost: {cost_time:.3f}")
+    success_users = []
+    for i, s in enumerate(success):
+        success_users.append(s)
+        if i > 0 and i % 4 == 0:
+            success_users.append("\n")
+    success_users = "".join(success_users)
+    failed_prompt = f"{'-'*20} FAILED {'-'*20}\n{'^'.join(failed)}\n" if failed else ""
+    title = f"{act.upper()} OK {gift_name} @{room_id}${gift_id}"
+    split_char_count = max(0, (80 - len(title)) // 2)
+    logging.info(
+        f"\n{'-'*split_char_count}{title}{'-'*split_char_count}\n"
+        f"{success_users}\n"
+        f"{failed_prompt}"
+        f"\nWorker: {index}, cloud_acceptor: {cloud_acceptor_url[-20:]}, total: {len(success)}\n"
+        f"{'-'*80}"
+    )
 
 
-async def main():
-    logging.info("-" * 80)
-    logging.info("LT ACCEPTOR started!")
-    logging.info("-" * 80)
-
-    async def nop(*args, **kw):
-        pass
-
+async def listen_ws():
     async def on_message(message):
         try:
             data = json.loads(message)
         except json.JSONDecodeError:
             return
-
-        raffle_type = data.get("raffle_type")
         now = int(time.time())
-        ts = data.get("ts", now)
 
-        if raffle_type in ("anchor", "storm"):
-            return
-        elif raffle_type == "tv":
+        raffle_type = data["raffle_type"]
+        ts = data["ts"]
+        room_id = data["real_room_id"]
+        raffle_id = data["raffle_id"]
+        gift_type = data.get("gift_type", "")
+        gift_name = data.get("gift_name", "")
+
+        if raffle_type == "tv":
+            act = "join_tv_v5"
             time_wait = data["time_wait"]
             max_time = data["max_time"]
             accept_start_time = ts + time_wait
@@ -200,39 +157,80 @@ async def main():
             wait_time = int((accept_end_time - accept_start_time - 8) * random.random())
             recommended_implementation_time = accept_start_time + wait_time
         elif raffle_type == "guard":
+            act = "join_guard"
             recommended_implementation_time = ts + random.randint(60, 600)
-        else:
+        elif raffle_type == "pk":
+            act = "join_pk"
             recommended_implementation_time = now
+        else:
+            return
 
-        print(f"received. delay: {recommended_implementation_time - ts:.0f} -> {data}.")
+        task_params = {
+            "act": act,
+            "room_id": room_id,
+            "gift_id": raffle_id,
+            "gift_type": gift_type,
+            "gift_name": gift_name,
+        }
+        await DelayAcceptGiftsQueue.put(task_params, recommended_implementation_time)
         await redis_cache.set("LT_LAST_ACTIVE_TIME", value=int(time.time()))
-        await DelayAcceptGiftsQueue.put(data, recommended_implementation_time)
+        logging.info(
+            f"RAFFLE received: {raffle_type} {room_id} $ {raffle_id}. "
+            f"delay: {recommended_implementation_time - ts:.0f} -> {data}."
+        )
 
-    new_client = RCWebSocketClient(
-        url="wss://www.madliar.com/raffle_wss",
-        on_message=on_message,
-        on_connect=nop,
-        on_shut_down=nop,
-        heart_beat_pkg="a",
-        heart_beat_interval=300
-    )
-    await new_client.start()
+    while True:
+        session = aiohttp.ClientSession()
+        async with session.ws_connect(url="wss://www.madliar.com/raffle_wss") as ws:
+            async for msg in ws:
+                if msg.type == aiohttp.WSMsgType.ERROR:
+                    break
+                try:
+                    await on_message(msg.data)
+                except Exception as e:
+                    logging.error(
+                        f"Error in proc single: {e}\n"
+                        f"`{msg.data}`\n"
+                        f"{traceback.format_exc()}"
+                    )
 
-    monitor_q = asyncio.Queue()
+        logging.warning(f"Ws Connection broken!")
 
-    async def select_task():
+
+async def interval_assign_task(task_q):
+    while True:
+        task_params = await DelayAcceptGiftsQueue.get()
+        for p in task_params:
+            task_q.put_nowait(p)
+
+        await asyncio.sleep(4)
+
+
+async def main():
+    logging.info("-" * 80)
+    logging.info("LT ACCEPTOR started!")
+    logging.info("-" * 80)
+
+    task_q = asyncio.Queue()
+
+    async def worker(index):
         while True:
-            raffle_tasks = await DelayAcceptGiftsQueue.get()
-            for t in raffle_tasks:
-                monitor_q.put_nowait(t)
-            await asyncio.sleep(4)
+            params = await task_q.get()
+            start_time = time.time()
+            try:
+                await accept(index, **params)
+            except Exception as e:
+                logging.error(f"ACCEPTOR worker[{index}] error: {e}\n{traceback.format_exc()}")
 
-    tasks = [asyncio.create_task(select_task())]
-    tasks += [
-        asyncio.create_task(Worker(100 + index, monitor_q).accept_delayed())
-        for index in range(128)
-    ]
-    await asyncio.gather(*tasks)
+            cost_time = time.time() - start_time
+            if cost_time > 5:
+                logging.warning(f"ACCEPTOR worker[{index}] exec long time: {cost_time:.3f}")
+
+    await asyncio.gather(
+        listen_ws(),
+        interval_assign_task(task_q),
+        *[worker(index) for index in range(128)]
+    )
 
 
 loop = asyncio.get_event_loop()
