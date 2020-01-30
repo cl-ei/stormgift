790bffff3688694161862381621341cbc13829b5
Caoliang <i@caoliang.net>
2020-01-30 14:09:20 +0800

    refine.

--- utils/highlevel_api.py
+++ utils/highlevel_api.py
@@ -71,7 +71,7 @@ class ReqFreLimitApi(object):
 
     @classmethod
     async def get_raffle_record(cls, uid):
-        url = "https://www.madliar.com/bili/raffles?day_range=7&json=1&user={uid}"
+        url = f"https://www.madliar.com/bili/raffles?day_range=7&json=1&user={uid}"
         async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=30)) as session:
             async with session.get(url) as resp:
                 response = await resp.json()
