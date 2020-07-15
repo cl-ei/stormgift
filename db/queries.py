7a290da680c07a2383a48c1c0823609f34b6eb6f
caoliang <caoliang@ainirobot.com>
2020-07-15 10:13:05 +0800

    修改登录问题。

--- db/queries.py
+++ db/queries.py
@@ -62,6 +62,8 @@ class LTUserQueryMixin:
             fields.append("bind_qq")
 
         lt_user = LTUser(**params)
+        from config.log4 import lt_login_logger as logging
+        logging.info(f"upsert lt user: {lt_user.DedeUserID}, fields: {fields}, params: {params}")
         await self.update_lt_user(lt_user, fields=fields)
 
         lt_user = await self.get_lt_user_by_uid(lt_user.user_id)
