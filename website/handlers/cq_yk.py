9f2d19d9a5b6278d252f2e8b208cbb8c5f268f70
caoliang <caoliang@ainirobot.com>
2020-10-09 16:29:08 +0800

    更新小心心直播间.

--- website/handlers/cq_yk.py
+++ website/handlers/cq_yk.py
@@ -7,6 +7,10 @@ from utils.cq import qq_yk
 from utils.images import get_random_image
 
 
+TIMES_LIMIT_K = f"LT_YK_LIMIT_{datetime.datetime.now().date()}"
+TIMES_LIMIT_V = 13
+
+
 async def _proc_one_sentence(group_id: int):
     key = f"LT_ONE_SENTENCE_{group_id}"
     is_second = False
@@ -21,6 +25,16 @@ async def _proc_one_sentence(group_id: int):
         else:
             return
 
+    count = await redis_cache.incr(TIMES_LIMIT_K)
+    if count == TIMES_LIMIT_V:
+        await qq_yk.send_group_msg(
+            group_id=group_id,
+            message=f"今日已响应召唤{TIMES_LIMIT_V}次，随即进入冬眠。（土豆出现即可重置次数限制"
+        )
+        return
+    if count > TIMES_LIMIT_V:
+        return
+
     async with aiohttp.request("get", "https://v1.hitokoto.cn/") as req:
         if req.status != 200:
             return ""
@@ -28,7 +42,7 @@ async def _proc_one_sentence(group_id: int):
         s = r.get("hitokoto") or ""
 
     if is_second:
-        s += f"{s}\n\n5分钟内不再响应."
+        s = f"{s}\n\n为防止刷屏，5分钟内不再响应."
     await qq_yk.send_group_msg(group_id=group_id, message=s)
 
 
@@ -42,6 +56,16 @@ async def _proc_one_image(group_id):
             is_second = True
         else:
             return
+
+    count = await redis_cache.incr(TIMES_LIMIT_K)
+    if count == TIMES_LIMIT_V:
+        await qq_yk.send_group_msg(
+            group_id=group_id,
+            message=f"今日已响应召唤{TIMES_LIMIT_V}次，随即进入冬眠。（土豆出现即可重置次数限制"
+        )
+        return
+    if count > TIMES_LIMIT_V:
+        return
 
     content = await get_random_image()
     if not content:
@@ -62,6 +86,12 @@ async def handler(request):
         return web.Response(text="", status=204)
     if context.get("message_type") != "group":
         return web.Response(text="", status=204)
+
+    sender = context["sender"]
+    user_id = sender["user_id"]
+
+    if user_id == 448942555:
+        await redis_cache.delete(TIMES_LIMIT_K)
 
     group_id = context["group_id"]
     msg = context["message"]
