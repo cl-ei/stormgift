c31f46394d2b51094433eba1ba3e108747d68e1e
caoliang <caoliang@ainirobot.com>
2020-12-28 15:43:02 +0800

    更新YK图。

--- website/handlers/cq_zy.py
+++ website/handlers/cq_zy.py
@@ -221,7 +221,7 @@ class BotUtils:
 
     async def proc_one_record(self):
         _ = self
-        return f"[CQ:record,file=/home/wwwroot/qq/records/rest.wav]"
+        return f"[CQ:record,file=/home/wwwroot/qq/plugins/record/rest.wav]"
 
     async def proc_song(self, msg):
         song_name = msg.split("点歌")[-1].strip()
