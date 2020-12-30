2d3a06a70931551a76c48a27d02a52b759794c56
caoliang <caoliang@ainirobot.com>
2020-12-30 14:22:18 +0800

    更新YK图。

--- website/handlers/mirai.py
+++ website/handlers/mirai.py
@@ -1,11 +1,12 @@
 import time
 import aiohttp
+from aiohttp.web_request import Request
 import datetime
 from aiohttp import web
 from utils.dao import redis_cache
 from config.log4 import cqbot_logger as logging
 
 
-async def handler(request):
-    print(f"request: {type(request)}")
+async def handler(request: Request):
+    print(f"request: {request.method}")
     return web.Response(text="", status=204)
